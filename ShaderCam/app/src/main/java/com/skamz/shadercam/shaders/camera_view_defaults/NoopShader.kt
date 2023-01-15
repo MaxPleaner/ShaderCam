c0883303bb21f9cffb60833b684a509f6177f69f
maxpleaner@gmail.com <maxpleaner@gmail.com>
2023-01-14 19:13:47 -0800

    refactor

--- ShaderCam/app/src/main/java/com/skamz/shadercam/shaders/camera_view_defaults/NoopShader.kt
+++ ShaderCam/app/src/main/java/com/skamz/shadercam/shaders/camera_view_defaults/NoopShader.kt
@@ -1,7 +1,7 @@
 package com.skamz.shadercam.shaders.camera_view_defaults
 
-import com.skamz.shadercam.shaders.util.AbstractShader.Companion.defaultShaderMainText
 import com.skamz.shadercam.shaders.util.ShaderAttributes
+import com.skamz.shadercam.shaders.util.defaultShaderMainText
 
 val NoopShader = ShaderAttributes(
     "Pass Through",

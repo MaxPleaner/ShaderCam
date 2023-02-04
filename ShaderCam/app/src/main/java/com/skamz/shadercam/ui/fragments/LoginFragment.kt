9e30cba7f71a407779abe01548f52498d1a75ec2
Darkknight123 <philipkerry57@gmail.com>
2023-02-04 23:18:37 +0300

    login screen implementation

--- ShaderCam/app/src/main/java/com/skamz/shadercam/ui/fragments/LoginFragment.kt
+++ ShaderCam/app/src/main/java/com/skamz/shadercam/ui/fragments/LoginFragment.kt
@@ -5,17 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.skamz.shadercam.R
+import com.skamz.shadercam.databinding.FragmentLoginBinding
 
 
 class LoginFragment : Fragment() {
+
+    private val TAG = "Login"
+
+    private lateinit var binding : FragmentLoginBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_login, container, false)
+
+        return binding.root
     }
 
 }

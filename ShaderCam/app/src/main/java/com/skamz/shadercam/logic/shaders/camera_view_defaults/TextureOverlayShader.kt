50c425cc61c8fb515367f88bcf404e5abe82e03d
maxpleaner@gmail.com <maxpleaner@gmail.com>
2023-02-11 10:43:36 -0800

    update default shader

--- ShaderCam/app/src/main/java/com/skamz/shadercam/logic/shaders/camera_view_defaults/TextureOverlayShader.kt
+++ ShaderCam/app/src/main/java/com/skamz/shadercam/logic/shaders/camera_view_defaults/TextureOverlayShader.kt
@@ -1,5 +1,6 @@
 package com.skamz.shadercam.logic.shaders.camera_view_defaults
 
+import android.net.Uri
 import com.skamz.shadercam.logic.shaders.util.*
 
 class TextureOverlayShaderData {
@@ -7,14 +8,17 @@ class TextureOverlayShaderData {
         val shaderMainText: String = """
         vec3 image(vec2 uv, vec3 color) {
             vec3 overlay = texture2D(overlayTexture, uv).rgb;
-            return color - (overlay * amt);
+            vec3 overlay2 = texture2D(overlayTexture2, uv).rgb;
+            return mix(mix(color, overlay, amt), overlay2, amt2);
         }    
         """.trimIndent()
         val defaultImageUrl = TextureUtils.resourceIdToUri("R.drawable.noise_texture").toString()
 
         val params: MutableList<ShaderParam> = mutableListOf(
             TextureShaderParam("overlayTexture", defaultImageUrl),
-            FloatShaderParam("amt", 0.75f, 0.0f, 1.0f)
+            TextureShaderParam("overlayTexture2", "https://y2w9c4y8.stackpathcdn.com/showimg_hhl59_full.jpg"),
+            FloatShaderParam("amt", 0.5f, 0.0f, 1.0f),
+            FloatShaderParam("amt2", 0.5f, 0.0f, 1.0f)
         )
     }
 }
